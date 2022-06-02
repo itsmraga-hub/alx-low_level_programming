81d1c08fc60594ef87b191ea75fe59a5822dc671
itsmraga-hub <itsragamit@gmail.com>
2022-06-02 08:44:05 +0300

    fixed 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-
+/* A c program that uses puts instead of print. */
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
