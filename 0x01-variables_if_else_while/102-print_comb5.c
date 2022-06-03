c461cf8dd41937d2486c6c546e52c1a81095fd04
itsmraga-hub <itsragamit@gmail.com>
2022-06-03 05:49:15 -0700

    fixed betty error

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,37 +7,38 @@
  */
 int main(void)
 {
-	int i, j, k, l, first, second;
 
-	for (i = 48; i < 58; i++)
+int i, j, k, l, first, second;
+
+for (i = 48; i < 58; i++)
+{
+	for (j = 48; j < 58; j++)
 	{
-		for (j = 48; j < 58; j++)
+		for (k = 48; k < 58; k++)
 		{
-			for (k = 48; k < 58; k++)
+			for (l = 48; l < 58; l++)
 			{
-				for (l = 48; l < 58; l++)
-				{
-					first = (i * 10) + j;
-					second = (k * 10) + l;
+				first = (i * 10) + j;
+				second = (k * 10) + l;
 
-					if (first < second)
-					{
-						putchar(i);
-						putchar(j);
-						putchar(' ');
-						putchar(k);
-						putchar(l);
-						if (i == 57 && j == 56 && k == 57 && l == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
+				if (first < second)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+						break;
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
+}
 
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
