2e473db228091b0ec05bc5168467020b0802e6d5
itsmraga-hub <itsragamit@gmail.com>
2022-06-07 14:31:19 +0300

    Fifth commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -23,4 +23,5 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
+	return (0);
 }
--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -19,6 +19,8 @@ int main(void)
     _putchar(r + '0');
     _putchar('\n');
 	*/
+
+	/**
 	
 	int r;
 
@@ -29,6 +31,30 @@ int main(void)
     r = _isalpha(108);
     _putchar(r + '0');
     r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+	*/
+
+	int r;
+
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
 	return (0);
