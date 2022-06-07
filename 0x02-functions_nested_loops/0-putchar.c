b095df87da6a985dc2c3a14a12503a0519a2df1e
itsmraga-hub <itsragamit@gmail.com>
2022-06-07 08:26:19 +0300

    First commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - A program that printf putchar, followed by a new line
+*
+* Return: (0) for success.
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* print_alphabet - A function that prints alphabet a to z
+*
+* Return: (0)
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 104; i++)
+	{
+		_putchar(i);
+	}
+}
