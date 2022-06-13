db0b5f85ea47ab7791cf2c2547e79fe3a26f76da
itsmraga-hub <itsragamit@gmail.com>
2022-06-13 17:42:16 +0300

    last

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,34 +8,34 @@
 
 int _atoi(char *s)
 {
-	unsigned int c, s, oi pn, m, i;
+	unsigned int c, size, oi pn, m, i;
 
 	c = 0;
-	s = 0;
+	size = 0;
 	oi = 0;
 	pn = 0;
 	m = 1;
 
-	while (*(s + c) != '\0')
+	while (*(size + c) != '\0')
 	{
-		if ((*(s + c) < '0' || *(s + c) > '9') && s > 0)
+		if ((*(size + c) < '0' || *(size + c) > '9') && size > 0)
 			break;
 
-		if (*(s + c) == '-')
+		if (*(size + c) == '-')
 			pn *= -1;
 
-		if ((*(s + c) >= '0') && (*(s + c) <= '9'))
+		if ((*(size + c) >= '0') && (*(size + c) <= '9'))
 		{
-			if (s > 0)
+			if (size > 0)
 				m *= 10;
-			s++;
+			size++;
 		}
 		c++;
 	}
 
-	for (i = c - s; i < c; i++)
+	for (i = c - size; i < c; i++)
 	{
-		oi = oi + ((*(s + i) - 48) * m);
+		oi = oi + ((*(size + i) - 48) * m);
 		m /= 10;
 	}
 
