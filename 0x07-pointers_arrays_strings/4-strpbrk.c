e10d74a769fbff63e1582c25d91df35b71dd6fa1
itsmraga-hub <itsragamit@gmail.com>
2022-06-20 17:25:50 +0300

    second commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -17,7 +17,7 @@ char *_strpbrk(char *s, char *accept)
 	while (*(s + i) != '\0')
 	{
 		j = 0;
-		
+
 		while (*(accept + j) != '\0')
 		{
 			if (*(s + i) == *(accept + j))
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,2 +1,33 @@
 #include "main.h"
 
+/**
+* _strstr - finds first occurence of the smaller string
+* @haystack: full string
+* @needle: smaller string to find
+*
+* Return: pointer to beginning of located string
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *stack, *sneedle;
+
+	while (*haystack != '\0')
+	{
+		stack = haystack;
+		sneedle = needle;
+
+		while (*haystack != '\0' && sneedle != '\0' && *haystack == *sneedle)
+		{
+			haystack++;
+			sneedle++;
+		}
+
+		if (!*sneedle)
+			return (stack);
+
+		haystack = stack + 1;
+	}
+
+	return (0);
+}
