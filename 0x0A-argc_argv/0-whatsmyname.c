66e3c73ddb2a02c2653e0964aaa0118103e11c2c
itsmraga-hub <itsragamit@gmail.com>
2022-06-24 14:38:37 +0300

    static libraries first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - program that prints its name
+* @argc: arguments
+* @argv: array of arguments
+*
+* Return: (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	print("%s\n", argv[0]);
+	return (0);
+}
