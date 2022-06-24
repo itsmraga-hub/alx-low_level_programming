a7815dc21b1e5ae98d63260d52a95f4e222f2cd4
itsmraga-hub <itsragamit@gmail.com>
2022-06-24 18:01:41 +0300

    second commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
 * Return: (0)
 */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,8 @@
 * Return: (0)
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
