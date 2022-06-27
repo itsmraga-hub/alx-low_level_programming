e05b28564a240d5b72472abedcde57194a0306dd
itsmraga-hub <itsragamit@gmail.com>
2022-06-27 17:49:54 +0300

    string concatanation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,5 +10,23 @@
 
 char *_strdup(char *str)
 {
+	char *s;
+	int i = 0, len;
 
+	for (i = 0; i != '\0'; i++)
+		len++;
+
+	s = malloc(sizeof(char) * len);
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + i) != '\0')
+	{
+		s[i] = *(str + i);
+		i++;
+	}
+
+	*(s + i) = '\0';
+	return (s);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,5 +11,43 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	char *str;
+	int i, j, len;
 
+	if (s2 == NULL || s1 == NULL)
+	{
+		if (s2 == NULL)
+			s2 = "";
+		else if (s1 == NULL)
+			s1 = "";
+	}
+
+	i = 0;
+	for (; i != '\0'; i++)
+		len++;
+
+	for (i = 0; i != '\0'; i++)
+		len++;
+
+	str = malloc(sizeof(char) * (len + 1));
+
+	j = 0;
+	i = 0;
+	while (*(s1 + j) != '\0')
+	{
+		str[i] = *(s1 + j);
+		j++;
+		i++;
+	}
+
+	j = 0;
+	while (*(s2 + j) != '\0')
+	{
+		str[i] = *(s2 + j);
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+
+	return (str);
 }
