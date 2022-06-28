3f27b1d0d063eaf196320f70055b60ffcab93aa0
itsmraga-hub <itsragamit@gmail.com>
2022-06-28 16:35:15 +0300

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,5 +11,44 @@
 
 char *argstostr(int ac, char **av)
 {
+	char *str;
+	int i, j, len, l;
 
+	if (ac == 0)
+		return (NULL);
+	i = 0;
+	len = 0;
+	for (; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
+
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			len++;
+			j++;
+		}
+		len++;
+	}
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	l = 0;
+	for (; l < len; j++, l++)
+	{
+		if (av[i][j] == '\0')
+		{
+			str[l] = '\n';
+			i++;
+			l++;
+			j = 0;
+		}
+		if (l < len - 1)
+			str[l] = av[i][j];
+	}
+	str[l] = '\0';
+	return (str);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,13 +25,22 @@ int **alloc_grid(int width, int height)
 	}
 
 	for (i = 0; i < height; i++)
+	{
 		grid[i] = malloc(sizeof(int) * width);
 
+		if (grid[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(grid[i]);
+
+			free(grid);
+			return (NULL);
+		}
+	}
+
 	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
 			grid[i][j] = init;
-	}
 
 	return (grid);
 }
