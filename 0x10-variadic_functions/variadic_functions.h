f6c77a874091a6c55f0cd9deae676fa2ed62b358
itsmraga-hub <itsragamit@gmail.com>
2022-07-07 12:57:15 +0300

    Initialized 0x10 variadic functions repository

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef variadic_functions_h_
+#define variadic_functions_h_
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
