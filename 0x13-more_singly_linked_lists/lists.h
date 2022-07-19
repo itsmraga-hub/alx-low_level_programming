cb07cec7fc456ca2d68b8958992c0777852f34b8
itsmraga-hub <itsragamit@gmail.com>
2022-07-19 23:55:54 +0300

    fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,7 +33,7 @@ typedef struct listp_s
 } listp_t;
 
 size_t print_listint(const listint_t *h);
-size_t(listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
