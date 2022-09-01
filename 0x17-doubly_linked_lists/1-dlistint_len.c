44cf48c292d5a260164813354cdc0f7d8484fb2f
itsmraga-hub <itsragamit@gmail.com>
2022-09-01 10:13:10 -0700

    First commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in
+ *
+ * @h: head
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
